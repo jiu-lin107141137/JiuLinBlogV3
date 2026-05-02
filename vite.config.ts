e16aeb41612ba860bc217ccfdacd53730c04d6af
Jiu-Lin <jam930725@gmail.com>
2026-05-02 15:05:31 +0800

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/JiuLinBlogV3/',
   plugins: [vue()],
   resolve: {
     alias: {
